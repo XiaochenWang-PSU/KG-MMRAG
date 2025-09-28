4bc85815df9731813ec568c93846addec8ae0231
hoangcaobao <caobaohoang03@yahoo.com.vn>
2025-09-28 11:38:27 -0700

    GPT Version of ScienceQA

---
+++ ScienceQA/data_demo.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset
+import matplotlib.pyplot as plt
+
+data = load_dataset('derek-thomas/ScienceQA', split='test') # choose the test set
+example = data[5]
+print("Question:", example["question"], "\n")
+print("Choices:", example["choices"])
+print("Text context:", example["hint"])
+
+# Display the image using matplotlib
+plt.imshow(example["image"])
+plt.axis('off')  # Turn off axis numbers
+plt.show()
+
+print("Answer:", example["choices"][example["answer"]], "\n")
+print("Lecture:", example["lecture"], "\n")
+print("Solution:", example["solution"])
